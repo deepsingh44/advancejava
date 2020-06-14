38b7d5aa51e035de6dc3eda38ca4d2f027557de0
Deep Singh <Deep Singh@DESKTOP-T0PSQD7>
2020-06-14 20:55:48 +0530

    Hello World using servlet.

---
+++ HelloWord/src/com/deepsingh44/blogspot/cont/First.java
@@ -0,0 +1,25 @@
+package com.deepsingh44.blogspot.cont;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class First extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pw = resp.getWriter();
+		pw.print("<h1>Hello Http Servlet Example</h1>");
+	}
+
+}
